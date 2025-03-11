ce09a5cfef6e9bc3a015bc5f68503eb6a57e4921
Bruno <brunooliveira.apps1@gmail.com>
2025-03-10 22:42:40 -0300

    input now appears above the label

--- src/app/components/task-box/task-box.component.spec.ts
+++ src/app/components/task-box/task-box.component.spec.ts
@@ -1,12 +1,14 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { of } from 'rxjs';
 
 import { TaskBoxComponent } from './task-box.component';
 import { TaskService } from 'src/app/service/task-service.service';
 import { TaskInterface } from 'src/app/types/taskInterface';
+import { ElementRef } from '@angular/core';
 
 class MockTaskService {
   toggleTaskStatus = jasmine.createSpy('toggleTaskStatus');
+  changeTaskName = jasmine.createSpy('changeTaskName');
   tasks$ = of([]);
 }
 
@@ -31,9 +33,77 @@ describe('TaskBoxComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should call toggleTaskStatus() on service with the correct task ID', () => {
+  it('should call the service to toggle the task status with the correct task ID', () => {
     const mockTask: TaskInterface = { id: '1', text: 'task name', isCompleted: false };
     component.toggleStatusInService(mockTask);
     expect(mockService.toggleTaskStatus).toHaveBeenCalledOnceWith(mockTask.id);
   })
+
+  it('should enable edit mode', () => {
+    const mockTask: TaskInterface = { id: '1', text: 'task name', isCompleted: false };
+    component.setTaskInEditMode(mockTask);
+    expect(component.isEditing).toBe(true);
+  })
+
+  it('should set editingTask to selected task', () => {
+    const mockTask: TaskInterface = { id: '1', text: 'task name', isCompleted: false };
+    component.setTaskInEditMode(mockTask);
+    expect(component.editingTask).toBe(mockTask);
+  })
+
+  it('should focus and select text in the edit input field', fakeAsync(() => {
+    const mockTask: TaskInterface = { id: '1', text: 'task name', isCompleted: false };
+
+    component.editInput = {
+      nativeElement: {
+        focus: jasmine.createSpy(), select: jasmine.createSpy()
+      }
+    } as ElementRef
+
+    component.setTaskInEditMode(mockTask);
+    tick(); //simulate timeout()
+
+    expect(component.editInput.nativeElement.focus).toHaveBeenCalled();
+    expect(component.editInput.nativeElement.select).toHaveBeenCalled();
+  }))
+
+  it('should call the service when changed task name is valid, set edit mode to false and set editing task to empty', () => {
+    const mockTask: TaskInterface = { id: '1', text: 'old name', isCompleted: false };
+    component.editingTask = mockTask;
+    component.editInput = {
+      nativeElement: { value: 'newName' }
+    } as ElementRef;
+
+    component.changeTaskNameInService();
+    expect(mockService.changeTaskName).toHaveBeenCalledOnceWith('1', 'newName');
+    expect(component.isEditing).toBe(false);
+    expect(component.editingTask).toEqual({ id: '', text: '', isCompleted: false });
+  })
+
+  it('should NOT call the service if the task name is the same, set edit mode to false and set editing task to empty', () => {
+    const mockTask: TaskInterface = { id: '1', text: 'old name', isCompleted: false };
+    component.editingTask = mockTask;
+    component.editInput = {
+      nativeElement: { value: 'old name' }
+    } as ElementRef;
+
+    component.changeTaskNameInService();
+    expect(mockService.changeTaskName).not.toHaveBeenCalled();
+    expect(component.isEditing).toBe(false);
+    expect(component.editingTask).toEqual({ id: '', text: '', isCompleted: false });
+  })
+
+  it('should NOT call the service if the task name is empty, set edit mode to false and set enditing task to empty', () => {
+    const mockTask: TaskInterface = { id: '1', text: 'old name', isCompleted: false };
+    component.editingTask = mockTask;
+    component.editInput = {
+      nativeElement: { value: '     ' }
+    } as ElementRef;
+
+    component.changeTaskNameInService();
+    expect(mockService.changeTaskName).not.toHaveBeenCalled();
+    expect(component.isEditing).toBe(false);
+    expect(component.editingTask).toEqual({ id: '', text: '', isCompleted: false });
+  })
+
 });
